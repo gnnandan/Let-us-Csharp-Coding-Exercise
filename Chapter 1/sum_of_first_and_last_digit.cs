3f7d4cb2152a4a41187d7eb71677dc63601c97c9
Nandan G N <gnnandan7@gmail.com>
2021-09-10 22:03:47 +0530

    Structured Files

---
+++ Chapter 1/sum_of_first_and_last_digit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sum_of_first_and_last_digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, first, last;
+            Console.Write("Enter 4 digit number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The 4 digit number: {num}");
+
+            // remember the logic
+            first = (num / 1000) % 10;
+            last = (num % 10);
+            Console.WriteLine($"The sum of {first} and {last} is {first + last}");
+        }
+    }
+}
